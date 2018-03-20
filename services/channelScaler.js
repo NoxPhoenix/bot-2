e623f760b980424a36600030be3d0c26a118ee12
NoxPhoenix <mr.mikyo@gmail.com>
2018-03-19 22:52:12 -0400

    add help command for users

--- services/channelScaler.js
+++ services/channelScaler.js
@@ -18,7 +18,7 @@ const Scaler = {
     const channelName = channel.name;
     const duplicates = [];
     return Promise.map(channel.guild.channels.array(), (c) => {
-      if (c.name.startsWith(channelName)) duplicates.push(c);
+      if (c.name.startsWith(baseChannelName(channelName))) duplicates.push(c);
       return Promise.resolve();
     })
       .then(() => duplicates);
@@ -39,7 +39,7 @@ const Scaler = {
   correctChannelNames (duplicates) {
     const sortedChannels = _.sortBy(duplicates, ['name']);
     const baseName = sortedChannels.shift().name;
-    for (let i = 0; i < sortedChannels.length; i++) {
+    for (let i = 0; i < sortedChannels.length; i += 1) {
       sortedChannels[i].setName(`${baseName} ${i + 2}`);
     }
   },
@@ -48,12 +48,11 @@ const Scaler = {
     return this.duplicatesCheck(channel)
       .then(duplicates => (
         channel.guild.createChannel(`${baseChannelName(channel.name)} ${duplicates.length + 1}`, 'voice')
-          .then(newChannel => Promise.all([addScalableChannel(newChannel.id), newChannel.setPosition(duplicates.length + 1)]))
+          .then(newChannel => Promise.all([addScalableChannel(newChannel.id), newChannel.setPosition(channel.calculatedPosition + 1)]))
       ));
   },
 
   scale (channel, threshold = 1) {
-    console.log('scaling...');
     return this.emptyDuplicates(channel)
       .then((emptyDupes) => {
         if (channel.members.size >= threshold && emptyDupes.length === 0) return this.duplicate(channel);
